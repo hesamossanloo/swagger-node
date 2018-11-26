9abb0725e5f9e66fb8694f22919490bfc522b060
Hesam <hesam.ossanloo@propellerhead.co.nzPropellerhead.p. 09 309 6595Propellerhead.p. 09 309 6595Propellerhead.p. 09 309 6595w. propellerhead.co.nz>
2018-11-27 00:37:21 +1300

    Rafctoring, PrettyCode, FindFiles

---
+++ hello-pts/api/controllers/file_system_controller.js
@@ -0,0 +1,119 @@
+/*jshint esversion: 6 */
+'use strict';
+var util = require('util');
+
+module.exports = {
+    allFS: getAllFiles,
+    dlSpToGrRq: requestToDownloadAFileFromSpacecraftToGround,
+};
+
+/*
+    file vars
+ */
+var filesList = [];
+var alina = [];
+var alq1 = [];
+var alq2 = [];
+
+var file_1 = {
+    "tid": "1",
+    "size": "121b",
+    "name": "test-file-1",
+    "permissions": "admin,dev",
+    "location": "GROUND"
+}
+var file_2 = {
+    "tid": "2",
+    "size": "1001b",
+    "name": "test-file-2",
+    "permissions": "admin",
+    "location": "SPACECRAFT"
+}
+var file_3_1 = {
+    "tid": "3",
+    "size": "1001b",
+    "name": "test-file-3_1",
+    "permissions": "admin",
+    "location": "GROUND"
+}
+var file_3_2 = {
+    "tid": "3",
+    "size": "205b",
+    "name": "test-file-3_2",
+    "permissions": "admin",
+    "location": "SPACECRAFT"
+}
+var file_3_3 = {
+    "tid": "3",
+    "size": "205b",
+    "name": "test-file-3_3",
+    "permissions": "admin",
+    "location": "SPACECRAFT"
+}
+
+alina["GROUND"] = [file_1, file_2, file_3_1, file_3_2, file_3_3];
+alina["SPACECRAFT"] = [file_1, file_2, file_3_1, file_3_2];
+alq1["GROUND"] = [file_1, file_3_1];
+alq1["SPACECRAFT"] = [file_1];
+alq2["GROUND"] = [file_2, file_3_2, file_3_3];
+alq2["SPACECRAFT"] = [file_2, file_3_2];
+
+filesList["1"] = alq1;
+filesList["2"] = alq2;
+filesList["3"] = alina;
+
+function findByTIDandLocation(tid, location) {
+    var spacecraftAllFiles = filesList[tid];
+    if (spacecraftAllFiles === undefined) {
+        return "Target ID not found!";
+    }
+
+    var retSpacecraftFilesBasedOnLocation = spacecraftAllFiles[location];
+    if (retSpacecraftFilesBasedOnLocation === undefined) {
+        return "Location not found!";
+    }
+    return retSpacecraftFilesBasedOnLocation;
+}
+
+/*
+  Param 1: a handle to the request object
+  Param 2: a handle to the response object
+ */
+function getAllFiles(req, res) {
+    // variables defined in the Swagger document can be referenced using req.swagger.params.{parameter_name}
+    var tid = req.swagger.params.tid.value || 'No tid provided';
+    var location = req.swagger.params.location.value || 'No location provided';
+
+    var retSpacecraftFilesBasedOnLocation = findByTIDandLocation(tid, location);
+
+    // this sends back a JSON response which is an json object
+    res.json(retSpacecraftFilesBasedOnLocation);
+}
+
+function requestToDownloadAFileFromSpacecraftToGround(req, res) {
+    // variables defined in the Swagger document can be referenced using req.swagger.params.{parameter_name}
+    var tid = req.swagger.params.tid.value || 'No tid provided';
+    var filename = req.swagger.params.filename.value || 'No filename provided';
+    var location = req.swagger.params.location.value || 'No location provided';
+
+    var spacecraftFilesBasedOnLocation = findByTIDandLocation(tid,location);
+
+    var foundItem = false;
+
+    if (spacecraftFilesBasedOnLocation.length !== undefined) {
+        if(spacecraftFilesBasedOnLocation.find(x => x.name === filename)){
+            foundItem = true;
+        }
+    }
+
+    let ret;
+
+    if(foundItem){
+        ret= "Good news, file exists!";
+    } else {
+        ret= 'Either Target ID not found!';
+    }
+
+    // this sends back a JSON response which is an json object
+    res.json(ret);
+}
